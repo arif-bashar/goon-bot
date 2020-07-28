6d611de1c46dd428776869ae25de1a05c196e16c
Arif Bashar <arifbasharx@gmail.com>
2020-07-28 13:05:27 -0500

    processing chatlogs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import Discord, { Client } from "discord.js";
 import { token } from "./Config";
 import commands from "./commands";
+import fs from "fs";
+const chatlogs = require('./chatlogs.json');
+
+fs.open('andrew-logs.txt', 'w', (err, file) => {});
+
+for (let i = 0; i < 300; i++) {
+  if (chatlogs.chats[i].author.id == "160229525404385280") {
+    // console.log(chatlogs.chats[i].content);
+    fs.appendFile('andrew-logs.txt', chatlogs.chats[i].content, (err) => {});
+  }
+
+}
 
 const client: Client = new Discord.Client();
 
