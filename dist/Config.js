df8b0d5c18b1fbde210bd726561655b990ce90ef
Arif Bashar <34924416+arif-bashar@users.noreply.github.com>
2021-03-03 17:40:55 -0600

    Delete Config.js

--- dist/Config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.botID = exports.iexToken = exports.token = void 0;
-exports.token = "NzA4ODU1MTE5NTA4NDA2Mzcz.Xrdaxg.Tta3PRE_fexg06it8ozu10xUzHg";
-exports.iexToken = "pk_a97f71f9929e448b8c9e26ab4a93f548";
-exports.botID = "708855119508406373";
